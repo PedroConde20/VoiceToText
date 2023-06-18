3af88881a8a7019b780a0c1a04afc4c473577a0f
onlyTheF <fabianmqw@gmail.com>
2023-06-18 10:51:41 -0400

    ahora si da

--- SANDBOX_OPENAI/backend.py
+++ SANDBOX_OPENAI/backend.py
@@ -12,7 +12,7 @@ import os
 
 app = Flask(__name__)
 
-OPENAI_KEY = "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"
+OPENAI_KEY = "" #INGRESAR KEY DE OPENAI
 OPENAI_API_URL = "https://api.openai.com/v1"
 
 
@@ -31,30 +31,29 @@ def post_audio_file():
     # You must complete the full implementation for this
     # primero se agarra el dato que necesitamos con el metodo get_data que viene en flask
     audioFile = request.get_data()
-    # con ayuda de base64 vamos a decodificar el archivo llegado
-    audioOut = base64.b64decode(audioFile)
+    decoded_text = audioFile.decode('utf-8')
+    json_data = json.loads(decoded_text)
+    mensaje = json_data['data']
+
+    print(mensaje)
+
+    audioOut = base64.b64decode(mensaje)
 
     # Ruta temporal para guardar el archivo
-    audio_file_path = f"VOZ/prueba.webm"
+    audio_file_path = f"VOZ\prueba.webm"
 
     # Guardar el archivo de audio
     with open(audio_file_path, 'wb') as audio_file:
         audio_file.write(audioOut)
-
-    # Convertir el archivo de audio a formato wav
-    wav_file_path = f"VOZ/prueba.wav"
-    audio = AudioSegment.from_file(audio_file_path, format='webm')
-    audio.export(wav_file_path, format='wav')
+        
 
     # Realizar la transcripción utilizando el archivo de audio
     openai.api_key = OPENAI_KEY
-    with open(wav_file_path, 'rb') as audio_file:
+    with open(audio_file_path, 'rb') as audio_file:
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-    # Eliminar el archivo 'wav' después de la transcripción
-    os.remove(wav_file_path)
-
-    return render_template("index.html", transcription=transcript["text"])
+    #os.remove(audio_file_path)
+    return transcript
 
 
 if __name__ == '__main__':
